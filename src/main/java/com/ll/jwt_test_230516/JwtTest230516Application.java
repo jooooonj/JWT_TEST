c6f850f629f77679ede98907c85c9b0eb9a1ccff
jooooonj <lee22424205@naver.com>
2023-05-16 15:47:30 +0900

    setting

---
+++ src/main/java/com/ll/jwt_test_230516/JwtTest230516Application.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_test_230516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTest230516Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTest230516Application.class, args);
+    }
+
+}
